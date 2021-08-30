d60437a46b455ecff5e2727673408a01d4c5f699
Rishabh Singh <ris8645208@gmail.com>
2021-08-30 16:07:32 +0530

    feat: add styled cells

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { TETROMINOS } from '../tetrominos';
+import { StyledCell } from './styles/StyledCell';
 
 const Cell = ({ type }) => {
     return (
-        <div>
-            cell
-        </div>
+        <StyledCell type={type} color={TETROMINOS[type].color}>cell</StyledCell>
     )
 }
 
