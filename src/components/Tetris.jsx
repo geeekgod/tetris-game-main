ec5f31c98db8a157753b439d2461c52a1229147b
Rishabh Singh <ris8645208@gmail.com>
2021-09-21 16:34:16 +0530

    style: add styled tetris wrapper to wrap the game stage

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -3,19 +3,24 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers'
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 const Tetris = () => {
     return (
         <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
-                </div>
-                <StartButton />
-            </aside>
+            <StyledTetrisWrapper>
+                <StyledTetris>
+                    <Stage stage={createStage()} />
+                    <aside>
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                        <StartButton />
+                    </aside>
+                </StyledTetris>
+            </StyledTetrisWrapper>
         </div>
     )
 }
