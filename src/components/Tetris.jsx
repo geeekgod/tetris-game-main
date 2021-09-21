45db92ea4f9a582835941a07716313fd59cb343c
Rishabh Singh <ris8645208@gmail.com>
2021-09-21 17:10:30 +0530

    chore: add custom hooks and game state in main tetris component

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers'
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
 
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
     return (
         <div>
             <StyledTetrisWrapper>
                 <StyledTetris>
                     <Stage stage={createStage()} />
                     <aside>
-                        <div>
-                            <Display text="Score" />
-                            <Display text="Rows" />
-                            <Display text="Level" />
-                        </div>
+                        {gameOver ? (
+                            <Display gameOver={gameOver} text="Game Over" />
+                        )
+                            : <div>
+                                <Display text="Score" />
+                                <Display text="Rows" />
+                                <Display text="Level" />
+                            </div>}
                         <StartButton />
                     </aside>
                 </StyledTetris>
