9d6b3dd5c8ba6a851ccd725880db7388d30adc35
Rishabh Singh <ris8645208@gmail.com>
2021-09-21 23:11:24 +0530

    feat: add rotate shifter on collision of tetromino

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { checkCollision, STAGE_WIDTH } from "../gameHelpers";
 import { randomTetromino, TETROMINOS } from "../tetrominos";
 
 export const usePlayer = () => {
@@ -25,6 +25,17 @@ export const usePlayer = () => {
     const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
     setPlayer(clonedPlayer);
   };
 
