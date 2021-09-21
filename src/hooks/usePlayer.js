32a49a8b276ab42aa7a05616fc8fbaefa3bad275
Rishabh Singh <ris8645208@gmail.com>
2021-09-21 20:57:30 +0530

    fix: fix no tetromino or empty space on landing

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,11 +1,11 @@
 import { useCallback, useState } from "react";
 import { STAGE_WIDTH } from "../gameHelpers";
-import { randomTetromino } from "../tetrominos";
+import { randomTetromino, TETROMINOS } from "../tetrominos";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     collided: false,
   });
 
